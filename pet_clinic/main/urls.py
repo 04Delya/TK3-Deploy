f71bf76810be5b38ce3f7be26887da3264f0daa5
Dermada <141832231+Dermada@users.noreply.github.com>
2025-04-29 01:02:02 +0700

    Page register abis dibuat tapi blom ada isinya

--- pet_clinic/main/urls.py
+++ pet_clinic/main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('register/', views.register_selection, name='register'),
+    path('register/individual/', views.register_individual, name='register_individual'),
+    path('register/company/', views.register_company, name='register_company'),
+    path('register/frontdesk/', views.register_frontdesk, name='register_frontdesk'),
+    path('register/vet/', views.register_vet, name='register_vet'),
+    path('register/nurse/', views.register_nurse, name='register_nurse'),
 ] 
--- pet_clinic/main/views.py
+++ pet_clinic/main/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'main/landing_page.html')
+
+def register_selection(request):
+    return render(request, 'registration/register.html')
+
+def register_individual(request):
+    return render(request, 'registration/register_individual.html')
+
+def register_company(request):
+    return render(request, 'registration/register_company.html')
+
+def register_frontdesk(request):
+    return render(request, 'registration/register_frontdesk.html')
+
+def register_vet(request):
+    return render(request, 'registration/register_vet.html')
+
+def register_nurse(request):
+    return render(request, 'registration/register_nurse.html')
