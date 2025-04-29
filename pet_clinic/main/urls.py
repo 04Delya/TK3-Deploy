39ed251126164fc1f6b21176349946897aa604a4
Delya04 <adellyaa04@gmail.com>
2025-04-29 23:27:32 +0700

    add to master from delya

--- pet_clinic/main/urls.py
+++ pet_clinic/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import landing_page, dashboard, update_password, update_profile
+from .views import landing_page, update_password, update_profile, dashboard_klien, dashboard_frontdesk, dashboard_dokterhewan, dashboard_perawat
 
 urlpatterns = [
     path('', landing_page, name='landing_page'),
-    path('dashboard/', dashboard, name="dashboard"),
     path('profile/update/', update_profile, name='update_profile'),
     path('password/update/', update_password, name='update_password'),
+    path('dashboard/klien/', dashboard_klien, name='dashboard_klien'),
+    path('dashboard/fdo/', dashboard_frontdesk, name='dashboard_frontdesk'),
+    path('dashboard/dokter/', dashboard_dokterhewan, name='dashboard_dokterhewan'),
+    path('dashboard/perawat/', dashboard_perawat, name='dashboard_perawat'),
 ]
