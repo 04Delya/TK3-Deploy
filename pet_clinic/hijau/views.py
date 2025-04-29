6477a3d0079100d1e1535a0ae868a0046b97ebc5
A. Sherwyn Fawwaz Nitisara Suthaputra <a.sherwyn@ui.ac.id>
2025-04-29 16:49:58 +0700

    done

--- pet_clinic/hijau/views.py
+++ pet_clinic/hijau/views.py
@@ -17,3 +17,25 @@ def update_kunjungan_view(request):
 
 def delete_kunjungan_view(request):
     return render(request, 'delete_kunjungan.html')
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def treatment_views(request, action):
+    if action == 'create':
+        return render(request, 'create_treatment.html')
+    elif action == 'update':
+        return render(request, 'update_treatment.html')
+    elif action == 'delete':
+        return render(request, 'delete_treatment.html')
+    else:
+        return HttpResponse('Invalid action')
+
+def kunjungan_views(request, action):
+    if action == 'create':
+        return render(request, 'create_kunjungan.html')
+    elif action == 'update':
+        return render(request, 'update_kunjungan.html')
+    elif action == 'delete':
+        return render(request, 'delete_kunjungan.html')
+    else:
+        return HttpResponse('Invalid action')
