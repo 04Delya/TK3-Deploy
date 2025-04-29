197206d448f0b749da4e55d7ee8a5bd2d60537ad
Delya04 <adellyaa04@gmail.com>
2025-04-29 16:29:50 +0700

    landing page on going

--- pet_clinic/main/views.py
+++ pet_clinic/main/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def landing_page(request):
-    return render(request, 'main/landing_page.html')
+     return render(request, 'landingpage.html')
+
+
+# def pet_create(request):
+#     if request.method == 'POST':
+#         form = PetForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('pet_list')
+#     else:
+#         form = PetForm()
+#     return render(request, 'main/pet_form.html', {'form': form})
